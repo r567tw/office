07d3269dee64d031e43a982d93cc89853043fd80
r567tw <r567tw@gmail.com>
2020-02-03 18:58:51 +0800

    add web crawler

---
+++ web_crawler/crawler.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+with open("data.json", "r") as reader:
+    jf = json.loads(reader.read())
+
+data = jf["data"][0]
+url = data["url"]
+
+request = requests.get(url)
+content = request.content
+soup = BeautifulSoup(content, "html.parser")
+
+container = soup.select(data["select"])
+
+# 接下來只是寫入result.txt檔案的事情
+file = open("result.txt", "w")
+for item in container:
+    if item:
+        value = item.get_text().strip()
+        print(value)
+        # break # for test
+        file.write("- [ ]{}\n".format(value))  # 檔案寫入
+file.close()
---
+++ web_crawler/readme.md
@@ -0,0 +1,2 @@
+1. ``cp data.json.example data.json``
+2. Change data.json 
