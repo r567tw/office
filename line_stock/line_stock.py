78c0bb948dd6dbc85de78299cf3b2eaf35f4565c
r567twiamfang <r567tw@outlook.com>
2020-04-07 10:58:32 +0800

    改進流程

--- line_stock/line_stock.py
+++ line_stock/line_stock.py
@@ -6,15 +6,16 @@ import time
 def get_setting():
     res = []
     try:
-        with open("stock.txt") as f:
+        with open("stock.txt",encoding = "utf-8") as f:
             data = f.readlines()
             # print('讀入: ',data)
             data.remove("股票代號, 期望買進價格 , 期望賣出價格\n")
             for line in data:
                 stock = line.split(",")
                 res.append([stock[0].strip(), float(stock[1]), float(stock[2])])
-    except:
+    except Error:
         print("stock.txt 讀取錯誤")
+        print(Error)
     return res
 
 
@@ -22,7 +23,10 @@ def get_setting():
 def get_price(stockid):
     stock = twstock.realtime.get(stockid)
     if stock["success"]:
-        return (stock["info"]["name"], float(stock["realtime"]["latest_trade_price"]))
+        trade_price = stock["realtime"]["latest_trade_price"]
+        if(trade_price == '-'):
+            trade_price = stock["realtime"]["high"]
+        return (stock["info"]["name"], float(trade_price))
     else:
         return (False, False)
 
@@ -33,9 +37,9 @@ def get_four_best(stockid):
     best = twstock.BestFourPoint(stock).best_four_point()
     if best:
         if best[0]:
-            info = "買進"
+            info = "買進: "
         else:
-            info = "賣出"
+            info = "賣出: "
         return (info, best[1])
     else:
         return (False, False)
@@ -60,17 +64,22 @@ stocks = get_setting()
 # print('結果： ',stock)
 
 for stock in stocks:
+    # print(stock)
     id, low, high = stock
     name, price = get_price(id)
     message = ""
-    if price <= low:
-        message += "買進(股票低於自己設定的" + str(low) + "元)\n"
-    elif price >= high:
-        message += "賣出(股票高於自己設定的" + str(high) + "元)\n"
-
-    act, why = get_four_best(id)
-    message += "[四大買賣點判斷]" + act + why
-
-    send_ifttt(name, price, message)
-    print("請等個10秒鐘，不然這支程式會被證交所ban掉嗚嗚嗚！")
-    time.sleep(10)
+    if(name == False and price == False):
+        message += "股票代號{}資料擷取錯誤".format(id)
+        send_ifttt(name, price, message)
+    else:
+        if price < low:
+            message += "買進(股票低於自己設定的" + str(low) + "元) \n"
+        elif price > high:
+            message += "賣出(股票高於自己設定的" + str(high) + "元) \n"
+        else:
+            message += "不操作\n"
+        act, why = get_four_best(id)
+        message += "\n[四大買賣點判斷]" + act + why
+        send_ifttt(name, price, message)
+    print("請等個15秒鐘，不然這支程式會被證交所ban掉嗚嗚嗚！")
+    time.sleep(15)
